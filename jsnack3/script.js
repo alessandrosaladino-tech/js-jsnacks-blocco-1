fb7cdbad66e2b0cd17cda586e2de173e261671ba
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-08-29 16:11:33 +0200

    Add
    Snack 3

---
+++ jsnack3/script.js
@@ -0,0 +1,16 @@
+/*
+SNACK 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+let sommaNumeri = 0;
+
+for (let i = 0; i < 10; i++) {
+    const userNumber = Number(prompt("Inserisci un numero"));
+
+    sommaNumeri += userNumber;
+    
+}
+
+console.log("Il risultato è :",  sommaNumeri); 
