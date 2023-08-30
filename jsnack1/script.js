a4a277e27c4479a1ce3a6c243e1f77cb031a8b6a
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-08-30 11:17:12 +0200

    Add
    adjustment to while

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -23,22 +23,30 @@ if (userNumber1 > userNumber2) {
 
 /* Ciclo while*/
 
-const userNumber1 = Number(prompt("Inserisci un numero"));
-const userNumber2 = Number(prompt("Inserisci un numero"));
+//const userNumber1 = Number(prompt("Inserisci un numero"));
+//const userNumber2 = Number(prompt("Inserisci un numero"));
 
+const  list = []
 
 let i = 0
 
 while (i < 2) {
    
-    if (userNumber1 > userNumber2) {
-        console.log(userNumber1, "Il primo numero è maggiore!");
-    } else if (userNumber2 > userNumber1) {
-        console.log(userNumber2), "Il secondo numero è maggiore!";
-    } else {
-        console.log("I due numeri sono uguali. Riprova!");
-        
-    }
+    const userNumber = Number(prompt("inserisci il numero"))
+    list.push(userNumber)
+
 
     i++
+    
+
+    
+}
+
+if (list[0] > list[1]) {
+    console.log(list[0], "Il primo numero è maggiore!");
+} else if (list[1] > list[0]) {
+    console.log(list[1]), "Il secondo numero è maggiore!";
+} else {
+    console.log("I due numeri sono uguali. Riprova!");
+    
 }
