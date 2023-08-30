cbf81b93994f9bcc4fe06a0683a867f85165e953
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-08-30 10:55:20 +0200

    Add
    while loop

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,7 +4,7 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
-const userNumber1 = prompt("Inserisci il primo numero")
+/*const userNumber1 = prompt("Inserisci il primo numero")
 const userNumber2 = prompt ("inserisci il secondo numero")
 
 
@@ -16,3 +16,29 @@ if (userNumber1 > userNumber2) {
     console.log("I due numeri sono uguali. Riprova!");
 }
 
+*/
+
+
+
+
+/* Ciclo while*/
+
+const userNumber1 = Number(prompt("Inserisci un numero"));
+const userNumber2 = Number(prompt("Inserisci un numero"));
+
+
+let i = 0
+
+while (i < 2) {
+   
+    if (userNumber1 > userNumber2) {
+        console.log(userNumber1, "Il primo numero è maggiore!");
+    } else if (userNumber2 > userNumber1) {
+        console.log(userNumber2), "Il secondo numero è maggiore!";
+    } else {
+        console.log("I due numeri sono uguali. Riprova!");
+        
+    }
+
+    i++
+}
