c858ff379d1a60fae8c5fda38bcc28923ba43e1b
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-08-30 12:25:47 +0200

    Add
    while loop

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,6 +4,7 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 */
 
+/*
 let sommaNumeri = 0;
 
 for (let i = 0; i < 10; i++) {
@@ -14,3 +15,24 @@ for (let i = 0; i < 10; i++) {
 }
 
 console.log("Il risultato è :",  sommaNumeri); 
+
+*/
+
+
+
+
+//Ciclo while
+let sum = 0
+let x = 0
+while (x < 10) {
+    const usernumber = Number(prompt("Inserisci un numero"))
+    console.log(usernumber);
+
+    if (isNaN(usernumber)) {
+        sum += usernumber
+
+        x++
+    } 
+}
+
+console.log(sum)
